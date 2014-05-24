7775aa96e16ea0423ff386b523fdb8ab06a5df6a
Coolcade283 <coolcade283@gmail.com>
2014-05-24 16:20:07 -0500

    Added bottom bar
    Added a bottom bar to the activity_main.xml

--- gen/com/challengeaccepted/challengeaccepted/R.java
+++ gen/com/challengeaccepted/challengeaccepted/R.java
@@ -24,7 +24,10 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int action_settings=0x7f080003;
+        public static final int imageButtonAdd=0x7f080000;
+        public static final int imageButtonSettings=0x7f080002;
+        public static final int imageButtonView=0x7f080001;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -34,8 +37,11 @@ public final class R {
     }
     public static final class string {
         public static final int action_settings=0x7f050001;
+        public static final int addButtonDesc=0x7f050003;
         public static final int app_name=0x7f050000;
         public static final int hello_world=0x7f050002;
+        public static final int settingsButtonDesc=0x7f050005;
+        public static final int viewButtonDesc=0x7f050004;
     }
     public static final class style {
         /** 
