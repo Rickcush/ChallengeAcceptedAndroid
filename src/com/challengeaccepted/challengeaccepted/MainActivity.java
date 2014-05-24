3a70955312811b37f96ee9d882a114b2a1ed8633
Coolcade283 <coolcade283@gmail.com>
2014-05-24 17:37:01 -0500

    Added scrollview to mainactivity, added more strings

--- src/com/challengeaccepted/challengeaccepted/MainActivity.java
+++ src/com/challengeaccepted/challengeaccepted/MainActivity.java
@@ -10,6 +10,11 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        if (savedInstanceState != null) {
+        	 
+    	}
+
     }
 
 
