7bb69820aa52310744a125d09435f5f50f006b36
CushCode <Ricky@Rickys-MacBook-Pro.local>
2014-05-24 16:28:13 -0500

    Change @string/HelloWorld to @string/title
    <string name="title">Challenge Accepted</string>

--- gen/com/challengeaccepted/challengeaccepted/R.java
+++ gen/com/challengeaccepted/challengeaccepted/R.java
@@ -39,8 +39,8 @@ public final class R {
         public static final int action_settings=0x7f050001;
         public static final int addButtonDesc=0x7f050003;
         public static final int app_name=0x7f050000;
-        public static final int hello_world=0x7f050002;
         public static final int settingsButtonDesc=0x7f050005;
+        public static final int title=0x7f050002;
         public static final int viewButtonDesc=0x7f050004;
     }
     public static final class style {
